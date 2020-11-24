cb60a5f7d1d14e59b5480732e78ee7bee71dd2d5
Pravat Bhusal <pravat.bhusal@gmail.com>
2020-11-24 14:45:42 -0600

    Add comment for game and menu container

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,10 +7,18 @@ import { Header } from './components/header';
 import { Game } from './components/game';
 import { Signup } from './components/signup';
 
+/**
+ * The game container will take the entire screen
+ * if no other flex elements are on the DOM.
+ */
 const GameContainer = styled.div`
   flex: 6;
 `;
 
+/**
+ * The menu container will take up a portion of the
+ * screen with the game container once it appears.
+ */
 const MenuContainer = styled.div`
   flex: 4;
 `;
