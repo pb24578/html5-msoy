daa0af4ee39b81b498e543865a9d35a5e7cf2f3b
Pravat Bhusal <pravat.bhusal@gmail.com>
2020-11-24 11:25:59 -0600

    Use eslint config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const App = () => {
+  const x = 5;
+  console.log(x);
+
   return (
     <BrowserRouter>
       <Switch>
-        <Route path='/'>
+        <Route path="/">
           <div>Hello World!</div>
         </Route>
       </Switch>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
