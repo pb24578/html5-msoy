c71a2e486ee9a50391cb6f7124aabd5773b87145
Pravat Bhusal <pravat.bhusal@gmail.com>
2020-11-24 11:27:00 -0600

    Fix app tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-const App = () => {
-  const x = 5;
-  console.log(x);
-
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/">
-          <div>Hello World!</div>
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  );
-};
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/">
+        <div>Hello World!</div>
+      </Route>
+    </Switch>
+  </BrowserRouter>
+);
 
 export default App;
