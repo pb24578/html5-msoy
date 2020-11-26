119670cf6a696e07d86b4a98efec6c8bdcda7b29
Pravat Bhusal <pravat.bhusal@gmail.com>
2020-11-26 17:10:28 -0600

    Use camel case renderer

--- server/server/settings.py
+++ server/server/settings.py
@@ -48,6 +48,15 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_RENDERER_CLASSES': (
+        'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
+        'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'djangorestframework_camel_case.parser.CamelCaseFormParser',
+        'djangorestframework_camel_case.parser.CamelCaseMultiPartParser',
+        'djangorestframework_camel_case.parser.CamelCaseJSONParser',
+    ),
 }
 
 MIDDLEWARE = [
