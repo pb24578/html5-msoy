fbdf25015905cf67e3fbce554a6f3bb19d4942e9
Pravat Bhusal <pravat.bhusal@gmail.com>
2020-12-05 11:17:33 -0600

    Remove unnecessary salt

--- client/src/components/login/actions.ts
+++ client/src/components/login/actions.ts
@@ -1,6 +1,6 @@
 import { createAsyncAction } from 'async-selector-kit';
 import sha256 from 'crypto-js/sha256';
-import { LocalStorage, Salt } from '../../shared/constants';
+import { LocalStorage } from '../../shared/constants';
 import { actions } from '../../shared/user/reducer';
 import { fetchLogin } from './api';
 
@@ -9,7 +9,7 @@ const { setUser } = actions;
 export const [login, loadingLogin, errorLogin] = createAsyncAction({
   id: 'login',
   async: (store, status) => async (email: string, password: string) => {
-    const hashedPassword = sha256(password + Salt).toString();
+    const hashedPassword = sha256(password).toString();
     const user = await fetchLogin(email, hashedPassword);
     localStorage.setItem(LocalStorage.Session, JSON.stringify(user.session));
     store.dispatch(setUser(user));
--- client/src/shared/constants.ts
+++ client/src/shared/constants.ts
@@ -17,15 +17,6 @@ export const SocketURI = `ws://${hostName}:8000`;
 export const AppName = 'HTML5 Whirled';
 
 /**
- * Salt used to send passwords from the client to the server.
- *
- * Note that this is still not secure since the user can view the
- * salt in the client-side, so that's why we perform more security
- * measurements in the server-side before storing the password.
- */
-export const Salt = 'IMoZ1t4Okte2';
-
-/**
  * Keys used for local storage.
  */
 export enum LocalStorage {
