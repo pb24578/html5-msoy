e1a95d11dd4f99ecd3e21325459d8e45a247659e
Pravat Bhusal <pravat.bhusal@gmail.com>
2020-12-05 11:01:34 -0600

    Hash passwords before sending them to the server

--- client/src/components/login/actions.ts
+++ client/src/components/login/actions.ts
@@ -1,4 +1,5 @@
 import { createAsyncAction } from 'async-selector-kit';
+import sha256 from 'crypto-js/sha256';
 import { LocalStorage } from '../../shared/constants';
 import { actions } from '../../shared/user/reducer';
 import { fetchLogin } from './api';
@@ -8,7 +9,8 @@ const { setUser } = actions;
 export const [login, loadingLogin, errorLogin] = createAsyncAction({
   id: 'login',
   async: (store, status) => async (email: string, password: string) => {
-    const user = await fetchLogin(email, password);
+    const hashedPassword = sha256(password).toString();
+    const user = await fetchLogin(email, hashedPassword);
     localStorage.setItem(LocalStorage.Session, JSON.stringify(user.session));
     store.dispatch(setUser(user));
     return user;
--- client/src/components/signup/actions.ts
+++ client/src/components/signup/actions.ts
@@ -1,4 +1,5 @@
 import { createAsyncAction } from 'async-selector-kit';
+import sha256 from 'crypto-js/sha256';
 import { LocalStorage } from '../../shared/constants';
 import { actions } from '../../shared/user/reducer';
 import { fetchSignup } from './api';
@@ -8,7 +9,8 @@ const { setUser } = actions;
 export const [signup, loadingSignup, errorSignup] = createAsyncAction({
   id: 'signup',
   async: (store, status) => async (username: string, email: string, password: string) => {
-    const user = await fetchSignup(username, email, password);
+    const hashedPassword = sha256(password).toString();
+    const user = await fetchSignup(username, email, hashedPassword);
     localStorage.setItem(LocalStorage.Session, JSON.stringify(user.session));
     store.dispatch(setUser(user));
     return user;
