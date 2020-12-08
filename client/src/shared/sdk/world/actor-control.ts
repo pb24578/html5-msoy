2e4245ac27cea8f70691edc908ce53fadd01d0cb
Pravat Bhusal <pravat.bhusal@gmail.com>
2020-12-08 13:41:49 -0600

    Program actor and avatar control

--- client/src/shared/sdk/world/actor-control.ts
+++ client/src/shared/sdk/world/actor-control.ts
@@ -1,3 +1,52 @@
 import { EntityControl } from '.';
 
-export class ActorControl extends EntityControl {}
+interface State {
+  name: string;
+}
+
+export class ActorControl extends EntityControl {
+  private states: State[] | null = null;
+  private currentState: State | null = null;
+
+  /**
+   * Registers the actor's states. The first state will be your "default" state.
+   *
+   * @param states An Array of states, each must be less than 64 characters.
+   */
+  public registerStates(states: string[]) {
+    const maxStateLength = 64;
+
+    const registeredStates = states.map((state) => {
+      if (state.length > maxStateLength) {
+        throw new Error(`States cannot be greater than ${maxStateLength} characters.`);
+      }
+      const registerState: State = {
+        name: state,
+      };
+      return registerState;
+    });
+
+    this.states = registeredStates;
+    this.currentState = registeredStates[0] || null;
+  }
+
+  /**
+   * Returns the current state.
+   */
+  public getState() {
+    if (!this.currentState) throw new Error('You must set a state.');
+    return this.currentState.name;
+  }
+
+  /**
+   * Set a state, the state must be in the registered states.
+   */
+  public setState(state: string) {
+    if (!this.states) throw new Error('You must register states first.');
+    const newState = this.states.find((currentState) => currentState.name === state);
+    if (!newState) {
+      throw new Error(`The state ${state} does not exist.`);
+    }
+    this.currentState = newState;
+  }
+}
--- client/src/shared/sdk/world/avatar-control.ts
+++ client/src/shared/sdk/world/avatar-control.ts
@@ -1,3 +1,31 @@
+import * as PIXI from 'pixi.js-legacy';
 import { ActorControl } from '.';
 
-export class AvatarControl extends ActorControl {}
+interface Action {
+  name: string;
+}
+
+export class AvatarControl extends ActorControl {
+  private actions: Action[] | null = null;
+
+  /**
+   * Registers the avatar's actions.
+   *
+   * @param actions An Array of actions, each must be less than 64 characters.
+   */
+  public registerActions(actions: string[]) {
+    const maxActionLength = 64;
+
+    const registeredActions = actions.map((action) => {
+      if (action.length > maxActionLength) {
+        throw new Error(`Actions cannot be greater than ${maxActionLength} characters.`);
+      }
+      const registerAction: Action = {
+        name: action,
+      };
+      return registerAction;
+    });
+
+    this.actions = registeredActions;
+  }
+}
