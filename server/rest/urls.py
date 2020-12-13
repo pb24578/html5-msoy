c5f4355651647948227c858364bbb25b61264177
Pravat Bhusal <pravat.bhusal@gmail.com>
2020-12-13 16:34:56 -0600

    Showcase media if debug is true

--- server/rest/urls.py
+++ server/rest/urls.py
@@ -3,10 +3,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
+media_path = []
+if settings.DEBUG is True:
+    media_path = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 urlpatterns = [
     path('session', views.SessionView.as_view()),
     path('login', views.LoginView.as_view()),
     path('signup', views.SignupView.as_view()),
     path('rooms', views.RoomsView.as_view()),
     path('profiles/<int:id>', views.ProfilesView.as_view()),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + media_path
