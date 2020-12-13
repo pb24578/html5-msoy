83858f2231dd9a01369384d9e50301f968a3f595
Pravat Bhusal <pravat.bhusal@gmail.com>
2020-12-13 15:39:02 -0600

    Specify URI cannot end in a slash

--- client/src/components/world/actions.ts
+++ client/src/components/world/actions.ts
@@ -92,9 +92,9 @@ export const [setParticipantMap] = createAsyncAction(
     // eslint-disable-next-line max-len
     async: (store, status, participantMap, stage, user) => async (participants: ParticipantPayload[]) => {
       participants.forEach(({ avatar }) => {
-        const mediaTexture = `${ContentURI}${avatar.texture}`;
-        if (avatar && !PIXI.Loader.shared.resources[mediaTexture]) {
-          PIXI.Loader.shared.add(mediaTexture);
+        const textureURL = ContentURI + avatar.texture;
+        if (avatar && !PIXI.Loader.shared.resources[textureURL]) {
+          PIXI.Loader.shared.add(textureURL);
         }
       });
 
--- client/src/shared/constants.ts
+++ client/src/shared/constants.ts
@@ -2,6 +2,8 @@ const hostName = window.location.hostname;
 
 /**
  * The URI for the REST API.
+ *
+ * The URI must NOT end in a slash.
  */
 export const RestURI = `http://${hostName}:8000`;
 
@@ -9,11 +11,15 @@ export const RestURI = `http://${hostName}:8000`;
  * The URI for static and media files. The app uses the same URI for static
  * and media files because it doesn't between those two contents and uses
  * the same hosting service for it.
+ *
+ * The URI must NOT end in a slash.
  */
 export const ContentURI = RestURI;
 
 /**
  * The URI for the web-socket connections.
+ *
+ * The URI must NOT end in a slash.
  */
 export const SocketURI = `ws://${hostName}:8000`;
 
