7299e718943a0222b80c5d6b563a799df61d55a5
Pravat Bhusal <pravat.bhusal@gmail.com>
2020-12-13 19:41:37 -0600

    Use comment for cors_serve

--- server/rest/views/cors_serve.py
+++ server/rest/views/cors_serve.py
@@ -4,15 +4,12 @@ from django.conf import settings
 
 def cors_serve(request, path, insecure=False, **kwargs):
     """
-    Serves static and media files using the CORS configurations.
+    Serves static and media files to allow access for CORS. This should only be ran in development.
+    For production, use a separate webserver to host the media and static files instead of Django.
     https://stackoverflow.com/questions/62665049/how-to-fix-cors-error-when-loading-static-file-served-by-django-runserver
     """
 
     kwargs.pop('document_root')
     response = serve(request, path, insecure=insecure, **kwargs)
-
-    # use the settings from Django CorsHeaders
-    if settings.CORS_ORIGIN_ALLOW_ALL:
-        response['Access-Control-Allow-Origin'] = '*'
-
+    response['Access-Control-Allow-Origin'] = '*'
     return response
